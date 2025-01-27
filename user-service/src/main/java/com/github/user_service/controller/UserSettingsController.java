cd6903422c9bafd2fe7b3b66a75ad01d6d1446d6
Sebastian Milczarek <milczse9725_aehit@students.vizja.pl>
2025-01-27 20:09:14 +0100

    test

--- user-service/src/main/java/com/github/user_service/controller/UserSettingsController.java
+++ user-service/src/main/java/com/github/user_service/controller/UserSettingsController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user_settings")
+@RequestMapping("/api/user-settings")
 public class UserSettingsController {
 
     @Autowired
