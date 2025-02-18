de384084ff2abeea763f0871b7a34d8381a8a347
Sebastian Milczarek <milczse9725_aehit@students.vizja.pl>
2025-02-18 20:16:54 +0100

    Test

--- user-service/src/main/java/com/github/user_service/entity/Language.java
+++
@@ -1,5 +0,0 @@
-package com.github.user_service.entity;
-
-public enum Language {
-    PL, ANG;
-}
---
+++ user-service/src/main/java/com/github/user_service/userSettings/entity/Language.java
@@ -0,0 +1,5 @@
+package com.github.user_service.userSettings.entity;
+
+public enum Language {
+    PL, ANG;
+}
