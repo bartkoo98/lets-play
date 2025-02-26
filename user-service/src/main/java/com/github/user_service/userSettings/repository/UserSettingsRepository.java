2dfdb9a379de99fbe64cf63a60490c5cc59952da
Sebastian Milczarek <milczse9725_aehit@students.vizja.pl>
2025-02-26 20:15:41 +0100

    F001-AddFlyway

--- user-service/src/main/java/com/github/user_service/userSettings/repository/UserSettingsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.user_service.userSettings.repository;
-
-import com.github.user_service.userSettings.entity.UserSettings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSettingsRepository extends JpaRepository<UserSettings, Long> {
-}
