653d002ef607595846755fbd17fcfcfb1331e8bf
Sebastian Milczarek <161923225+jakundaf@users.noreply.github.com>
2025-04-07 22:01:27 +0200

    F002-UserGameProfile Auth Token maintenance (#6)

---
+++ user-service/src/main/resources/db/migration/V5__insert_sample_user.sql
@@ -0,0 +1,15 @@
+INSERT INTO User (
+    id, keycloakUUID, username, sex, age, avatar, description, city, theme, notification, appLanguage
+) VALUES (
+    1,
+    '330b05ea-17aa-4020-843f-16860a2cd561',
+    'ziutek',
+    'MALE',
+    18,
+    'avater',
+    'descp',
+    'WROCLAW',
+    true,
+    true,
+    'ENG'
+);
