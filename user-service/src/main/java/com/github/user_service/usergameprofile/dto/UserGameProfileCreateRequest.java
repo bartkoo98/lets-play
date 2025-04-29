dfe81d3e43d9e81368becafae6334ac7428e1b41
Sebastian Milczarek <161923225+jakundaf@users.noreply.github.com>
2025-04-29 21:31:44 +0200

    F002-UserGameProfile adding lombok and code refactoring (#10)

--- user-service/src/main/java/com/github/user_service/usergameprofile/dto/UserGameProfileCreateRequest.java
+++ user-service/src/main/java/com/github/user_service/usergameprofile/dto/UserGameProfileCreateRequest.java
@@ -3,7 +3,13 @@ package com.github.user_service.usergameprofile.dto;
 import com.github.user_service.usergameprofile.entity.Game;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserGameProfileCreateRequest {
     @Enumerated(EnumType.STRING)
     private Game game;
@@ -13,62 +19,5 @@ public class UserGameProfileCreateRequest {
     private String peak;
     private String accountLink;
 
-    public Game getGame() {
-        return game;
-    }
-
-    public void setGame(Game game) {
-        this.game = game;
-    }
-
-    public String getElo() {
-        return elo;
-    }
-
-    public void setElo(String elo) {
-        this.elo = elo;
-    }
-
-    public String getApproach() {
-        return approach;
-    }
-
-    public void setApproach(String approach) {
-        this.approach = approach;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getPeak() {
-        return peak;
-    }
-
-    public void setPeak(String peak) {
-        this.peak = peak;
-    }
-
-    public String getAccountLink() {
-        return accountLink;
-    }
-
-    public void setAccountLink(String accountLink) {
-        this.accountLink = accountLink;
-    }
-
-    public UserGameProfileCreateRequest(Game game, String elo, String approach, String role, String peak, String accountLink) {
-
-        this.game = game;
-        this.elo = elo;
-        this.approach = approach;
-        this.role = role;
-        this.peak = peak;
-        this.accountLink = accountLink;
-    }
 
 }
--- user-service/src/main/java/com/github/user_service/usergameprofile/entity/UserGameProfile.java
+++ user-service/src/main/java/com/github/user_service/usergameprofile/entity/UserGameProfile.java
@@ -1,9 +1,14 @@
 package com.github.user_service.usergameprofile.entity;
 
 import com.github.user_service.user.entity.User;
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import lombok.*;
 
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "UserGameProfile")
 public class UserGameProfile {
@@ -23,82 +28,4 @@ public class UserGameProfile {
     @JoinColumn(name = "user_id", nullable = true)
     private User user;
 
-    public UserGameProfile(Long id, Game game, String elo, String approach, String role, String peak, String accountLink, User user) {
-        this.id = id;
-        this.game = game;
-        this.elo = elo;
-        this.approach = approach;
-        this.role = role;
-        this.peak = peak;
-        this.accountLink = accountLink;
-        this.user = user;
-    }
-    public UserGameProfile(){
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Game getGame() {
-        return game;
-    }
-
-    public void setGame(Game game) {
-        this.game = game;
-    }
-
-    public String getElo() {
-        return elo;
-    }
-
-    public void setElo(String elo) {
-        this.elo = elo;
-    }
-
-    public String getApproach() {
-        return approach;
-    }
-
-    public void setApproach(String approach) {
-        this.approach = approach;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getPeak() {
-        return peak;
-    }
-
-    public void setPeak(String peak) {
-        this.peak = peak;
-    }
-
-    public String getAccountLink() {
-        return accountLink;
-    }
-
-    public void setAccountLink(String accountLink) {
-        this.accountLink = accountLink;
-    }
-
-    @Nullable
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(@Nullable User user) {
-        this.user = user;
-    }
 }
